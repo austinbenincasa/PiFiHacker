e10960dc96a6234496613b8db9cf5d97ce849ccb
austin <austinbenincasa@yahoo.com>
2016-08-31 14:12:52 -0400

    "added in port scanning"

--- map_network.py
+++ map_network.py
@@ -8,8 +8,9 @@ class map_network():
 
     def __init__(self):
         self.req_var = True
-        self.opt_var = False
+        self.opt_var = True
         self.req_variables = ["-n"]
+        self.opt_variables = ["-a"]
 
     def help(self):
         hlp = (
@@ -23,6 +24,15 @@ class map_network():
             "\n'map-network -n 192.168.1.*'\n"
         )
         return hlp
+
+    def is_up(self,ip):
+        """ Tests if host is up """
+        icmp = IP(dst=ip)/ICMP()
+        resp = sr1(icmp, timeout=10, verbose = False)
+        if resp == None:
+            return False
+        else:
+            return True
 
     def map_network(self, var):
         norm_class = normalize_output()
@@ -47,3 +57,38 @@ class map_network():
                 output.append("1")
                 output.append(e)
                 return output
+
+        if "-a" in var:
+            try:
+                if self.is_up(var["-a"]):
+                    closed_ports = 0
+                    open_ports = []
+                    conf.verb = 0 # Disable verbose in sr(), sr1() methods
+                    start_time = time.time()
+                    ports = range(1, 1024)
+                    print "Host %s is up, start scanning" % var["-a"]
+                    for port in ports:
+                        src_port = RandShort() # Getting a random port as source port
+                        p = IP(dst=var["-a"])/TCP(sport=src_port, dport=port, flags='S') # Forging SYN packet
+                        resp = sr1(p, timeout=2) # Sending packet
+                        if str(type(resp)) == "<type 'NoneType'>":
+                            closed_ports += 1
+                        elif resp.haslayer(TCP):
+                            if resp.getlayer(TCP).flags == 0x12:
+                                send_rst = sr(IP(dst=var["-a"])/TCP(sport=src_port, dport=port, flags='AR'), timeout=1)
+                                open_ports.append(port)
+                            elif resp.getlayer(TCP).flags == 0x14:
+                                closed_ports += 1
+                    duration = time.time()-start_time
+                    print "%s Scan Completed in %fs" % (var["-a"], duration)
+                    if len(open_ports) != 0:
+                        for pop in open_ports:
+                            print "%d open" % pop
+                else:
+                    print "Host %s is Down" % var["-a"]
+                            
+            except Exception, e:
+                output = []
+                output.append("1")
+                output.append(e)
+                print output
